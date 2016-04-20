4e291f48e28c407e81f9be4b5400d1b7874818a2
TakTraum_cp <SND\TakTraum_cp>
2016-04-20 21:23:03 +0000

    Added dropping of files and folders onto application window to open TSI files. Dropping folders opens contained files recursively.

---
+++ cmdr/cmdr.Editor/Behaviors/AttachedBehaviors.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace cmdr.Editor.Behaviors
+{
+    public static class AttachedBehaviors
+    {
+        public static readonly DependencyProperty DragDropBehaviourProperty =
+            DependencyProperty.RegisterAttached("DragDropBehaviour", typeof(ICommand), typeof(AttachedBehaviors),
+                new FrameworkPropertyMetadata(null,
+                    FrameworkPropertyMetadataOptions.None,
+                    OnDragDropBehaviourChanged));
+
+        public static ICommand GetDragDropBehaviour(DependencyObject d)
+        {
+            return (ICommand)d.GetValue(DragDropBehaviourProperty);
+        }
+
+        public static void SetDragDropBehaviour(DependencyObject d, ICommand value)
+        {
+            d.SetValue(DragDropBehaviourProperty, value);
+        }
+
+        private static void OnDragDropBehaviourChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UIElement g = d as UIElement;
+            if (g != null)
+            {
+                g.Drop += (s, a) =>
+                {
+                    ICommand iCommand = GetDragDropBehaviour(d);
+                    if (iCommand != null)
+                    {
+                        if (iCommand.CanExecute(a.Data))
+                        {
+                            iCommand.Execute(a.Data);
+                        }
+                    }
+                };
+            }
+            else
+            {
+                throw new ApplicationException("Non UIElement");
+            }
+        }
+    }
+}
