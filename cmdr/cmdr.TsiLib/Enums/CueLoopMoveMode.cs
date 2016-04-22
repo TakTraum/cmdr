fbb450576910e322b786616592e05dd2c1cf447e
TakTraum_cp <31767169+TakTraum@users.noreply.github.com>
2016-04-22 18:19:32 +0000

    [fix] Context Menu builder for conditions (some entries were dropped, although algorithm was ok for commands). Now it's much simpler. Added condition "Cue/Loop Move Mode" and corresponding enumeration.

---
+++ cmdr/cmdr.TsiLib/Enums/CueLoopMoveMode.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace cmdr.TsiLib.Enums
+{
+    public enum CueLoopMoveMode
+    {
+        Beatjump = 0,
+        
+        Loop = 1,
+        
+        [Description("Loop In")]
+        LoopIn = 2,
+
+        [Description("Loop Out")]
+        LoopOut = 3
+    }
+}
