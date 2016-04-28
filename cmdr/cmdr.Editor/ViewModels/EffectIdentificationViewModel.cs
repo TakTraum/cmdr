92ce50ae6b963b14ef903cda2dba760848856e83
TakTraum_cp <31767169+TakTraum@users.noreply.github.com>
2016-04-28 20:06:21 +0000

    Added handling of TSI files that contain effect selector commands but do not specify an effect list/snapshots Added a request event if this case occurs, that triggers a custom dialog where the user can chose how to solve this problem. It is also possible to ignore the affected commands and edit the file anyway

---
+++ cmdr/cmdr.Editor/ViewModels/EffectIdentificationViewModel.cs
@@ -0,0 +1,120 @@
+﻿using cmdr.Editor.AppSettings;
+using cmdr.Editor.Utils;
+using cmdr.TsiLib;
+using cmdr.TsiLib.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace cmdr.Editor.ViewModels
+{
+    public class EffectIdentificationViewModel : ViewModelBase
+    {
+        public enum Options
+        {
+            Option1,
+            Option2,
+            Option3
+        }
+
+        private readonly EffectIdentificationRequest _request;
+
+        public Action CloseAction { get; set; }
+        public Window Window { get; set; }
+
+        #region Commands 
+
+        private ICommand _closeCommand;
+        public ICommand CloseCommand
+        {
+            get { return _closeCommand ?? (_closeCommand = new CommandHandler(CloseAction)); }
+        }
+
+        private ICommand _okCommand;
+        public ICommand OkCommand
+        {
+            get { return _okCommand ?? (_okCommand = new CommandHandler(ok)); }
+        }
+        
+        private ICommand _browseFileCommand;
+        public ICommand BrowseFileCommand
+        {
+            get { return _browseFileCommand ?? (_browseFileCommand = new CommandHandler<TextBlock>((tb) => browseFile(tb))); }
+        }
+
+        #endregion
+
+        public bool CanUseTraktorSettings { get { return TraktorSettings.Initialized; } }
+
+        public string Title { get { return _request.Id; } }
+
+        private Options _currentOption;
+        public Options CurrentOption
+        {
+            get { return _currentOption; }
+            set { _currentOption = value; raisePropertyChanged("CurrentOption"); }
+        }
+
+        private string _pathToTsi;
+        public string PathToTsi
+        {
+            get { return _pathToTsi; }
+            set { _pathToTsi = value; raisePropertyChanged("PathToTsi"); }
+        }
+
+
+        public EffectIdentificationViewModel(EffectIdentificationRequest request)
+        {
+            _request = request;
+            _currentOption = Options.Option3;
+        }
+
+
+        private void ok()
+        {
+            switch (CurrentOption)
+            {
+                case EffectIdentificationViewModel.Options.Option1:
+                    _request.FxSettings = TraktorSettings.Instance.FxSettings;
+                    break;
+                case EffectIdentificationViewModel.Options.Option2:
+                    if (String.IsNullOrEmpty(PathToTsi))
+                        MessageBoxHelper.ShowError("Please specify a path.");
+                    else
+                    {
+                        try
+                        {
+                            _request.FxSettings = TsiFile.Load(CmdrSettings.Instance.TraktorVersion, PathToTsi).FxSettings;
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine(ex.Message);
+                            MessageBoxHelper.ShowError("Could not load " + PathToTsi);
+                        }
+                    }
+                    break;
+                case EffectIdentificationViewModel.Options.Option3:
+                    break;
+                default:
+                    break;
+            }
+
+            if (CloseAction != null)
+                CloseAction();
+        }
+
+        private void browseFile(TextBlock textBlock)
+        {
+            string folder = BrowseDialogHelper.BrowseTsiFile(Window, false);
+            if (folder != null)
+                textBlock.Text = folder;
+        }
+
+    }
+}
