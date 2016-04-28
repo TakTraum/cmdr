ba616d165ccef8dd2a95447823b0ec047f8df353
TakTraum_cp <SND\TakTraum_cp>
2016-04-28 21:11:43 +0000

    Some cosmetics for AppSettingsWindow and MessageBoxes Slightly modified welcome dialog

--- cmdr/cmdr.Editor/Utils/MessageBoxHelper.cs
+++ cmdr/cmdr.Editor/Utils/MessageBoxHelper.cs
@@ -1,37 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Interop;
 
 namespace cmdr.Editor.Utils
 {
     static class MessageBoxHelper
     {
-        public static void ShowInfo(string message)
+        static MessageBoxHelper()
         {
-            MessageBox.Show(App.Current.MainWindow, message, "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            // center message boxes
+            MessageBoxHelper.PrepToCenterMessageBoxOnForm(App.Current.MainWindow);
         }
 
-        public static void ShowWarning(string message)
+        public static void ShowInfo(string message, string title = null)
         {
-            MessageBox.Show(App.Current.MainWindow, message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            MessageBox.Show(App.Current.MainWindow, message, title ?? "Info", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
-        public static void ShowError(string message)
+        public static void ShowWarning(string message, string title = null)
         {
-            MessageBox.Show(App.Current.MainWindow, message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            MessageBox.Show(App.Current.MainWindow, message, title ?? "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
 
-        public static bool ShowQuestion(string message)
+        public static void ShowError(string message, string title = null)
         {
-            return MessageBox.Show(App.Current.MainWindow, message, "Question", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes;
+            MessageBox.Show(App.Current.MainWindow, message, title ?? "Error", MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
-        public static bool? ShowCancellableQuestion(string message)
+        public static bool ShowQuestion(string message, string title = null)
         {
-            var result = MessageBox.Show(App.Current.MainWindow, message, "Question", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
+            return MessageBox.Show(App.Current.MainWindow, message, title ?? "Question", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes;
+        }
+
+        public static bool? ShowCancellableQuestion(string message, string title = null)
+        {
+            var result = MessageBox.Show(App.Current.MainWindow, message, title ?? "Question", MessageBoxButton.YesNoCancel, MessageBoxImage.Question);
             switch (result)
             {
                 case MessageBoxResult.Yes:
@@ -42,5 +50,92 @@ namespace cmdr.Editor.Utils
                     return null;
             }
         }
+
+
+        #region MessageBoxCenterHelper by Linda Lawton
+
+        /// <summary>
+        /// The following code ist taken from Linda Lawton http://www.daimto.com/wpf-center-message-box-on-window/
+        /// </summary>
+        /// <param name="form"></param>
+
+        internal static void PrepToCenterMessageBoxOnForm(Window form)
+        {
+            MessageBoxCenterHelper helper = new MessageBoxCenterHelper();
+            helper.Prep(form);
+        }
+
+        private class MessageBoxCenterHelper
+        {
+            private int messageHook;
+            private IntPtr parentFormHandle;
+
+            public void Prep(Window form)
+            {
+                NativeMethods.CenterMessageCallBackDelegate callBackDelegate = new NativeMethods.CenterMessageCallBackDelegate(CenterMessageCallBack);
+                GCHandle.Alloc(callBackDelegate);
+
+                parentFormHandle = new WindowInteropHelper(form).Handle;
+                messageHook = NativeMethods.SetWindowsHookEx(5, callBackDelegate, new IntPtr(NativeMethods.GetWindowLong(parentFormHandle, -6)), NativeMethods.GetCurrentThreadId()).ToInt32();
+            }
+
+            private int CenterMessageCallBack(int message, int wParam, int lParam)
+            {
+                NativeMethods.RECT formRect;
+                NativeMethods.RECT messageBoxRect;
+                int xPos;
+                int yPos;
+
+                if (message == 5)
+                {
+                    NativeMethods.GetWindowRect(parentFormHandle, out formRect);
+                    NativeMethods.GetWindowRect(new IntPtr(wParam), out messageBoxRect);
+
+                    xPos = (int)((formRect.Left + (formRect.Right - formRect.Left) / 2) - ((messageBoxRect.Right - messageBoxRect.Left) / 2));
+                    yPos = (int)((formRect.Top + (formRect.Bottom - formRect.Top) / 2) - ((messageBoxRect.Bottom - messageBoxRect.Top) / 2));
+
+                    NativeMethods.SetWindowPos(wParam, 0, xPos, yPos, 0, 0, 0x1 | 0x4 | 0x10);
+                    NativeMethods.UnhookWindowsHookEx(messageHook);
+                }
+
+                return 0;
+            }
+        }
+
+        private static class NativeMethods
+        {
+            internal struct RECT
+            {
+                public int Left;
+                public int Top;
+                public int Right;
+                public int Bottom;
+            }
+
+            internal delegate int CenterMessageCallBackDelegate(int message, int wParam, int lParam);
+
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool UnhookWindowsHookEx(int hhk);
+
+            [DllImport("user32.dll", SetLastError = true)]
+            internal static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+            [DllImport("kernel32.dll")]
+            internal static extern int GetCurrentThreadId();
+
+            [DllImport("user32.dll", SetLastError = true)]
+            internal static extern IntPtr SetWindowsHookEx(int hook, CenterMessageCallBackDelegate callback, IntPtr hMod, int dwThreadId);
+
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool SetWindowPos(int hWnd, int hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
+
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        }
+
+        # endregion
     }
 }
