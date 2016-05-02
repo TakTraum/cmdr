e7f29625ba1ce54317dcad71a86cf17a55103337
TakTraum_cp <SND\TakTraum_cp>
2016-05-02 22:20:25 +0000

    Added static manager class for default mappings: async loading, caching, indexer. Can be used as a lookup for proprietary device "Notes".

---
+++ cmdr/cmdr.Editor/Utils/SerializationHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace cmdr.Editor.Utils
+{
+    public static class SerializationHelper
+    {
+        public static T Deserialize<T>(XDocument doc, bool useDataContract = false)
+        {
+            if (!useDataContract)
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+
+                using (var reader = doc.Root.CreateReader())
+                {
+                    return (T)xmlSerializer.Deserialize(reader);
+                }
+            }
+            else
+            {
+                DataContractSerializer xmlSerializer = new DataContractSerializer(typeof(T));
+
+                using (var reader = doc.Root.CreateReader())
+                {
+                    return (T)xmlSerializer.ReadObject(reader);
+                }
+            }
+        }
+
+        public static XDocument Serialize<T>(T value, bool useDataContract = false)
+        {
+            XDocument doc = new XDocument(new XDeclaration("1.0", "UTF-8", "no"));
+            if (!useDataContract)
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+                using (var writer = doc.CreateWriter())
+                    xmlSerializer.Serialize(writer, value);
+            }
+            else
+            {
+                DataContractSerializer xmlSerializer = new DataContractSerializer(typeof(T));
+
+                using (var writer = doc.CreateWriter())
+                    xmlSerializer.WriteObject(writer, value);
+            }
+
+            foreach (XElement XE in doc.Root.DescendantsAndSelf())
+            {
+                // Stripping the namespace by setting the name of the element to it's localname only
+                XE.Name = XE.Name.LocalName;
+                // replacing all attributes with attributes that are not namespaces and their names are set to only the localname
+                XE.ReplaceAttributes((from xattrib in XE.Attributes().Where(xa => !xa.IsNamespaceDeclaration) select new XAttribute(xattrib.Name.LocalName, xattrib.Value)));
+            }
+
+            return doc;
+        }
+    }
+}
---
+++ cmdr/cmdr.Editor/ViewModels/ControllerDefaultMappings.cs
@@ -0,0 +1,230 @@
+﻿using cmdr.Editor.AppSettings;
+using cmdr.TsiLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Runtime.Serialization;
+using cmdr.Editor.Utils;
+using System.Diagnostics;
+
+namespace cmdr.Editor.ViewModels
+{
+    [KnownType(typeof(ControllerDefaultMappingFile))]
+    [CollectionDataContract(Name = "DefaultMappings", Namespace="")]
+    public class ControllerDefaultMappings : List<ControllerDefaultMappings.ControllerDefaultMappingFile>
+    {
+
+        [DataContract(Name = "Mapping", Namespace="")]
+        public class ControllerDefaultMapping
+        {
+            [DataMember]
+            public string DeviceTypeStr { get; private set; }
+
+            public ControllerDefaultMappingFile File { get; set; }
+            public Device DefaultDevice { get; private set; }
+
+
+            public ControllerDefaultMapping(ControllerDefaultMappingFile file, string deviceTypeStr)
+            {
+                File = file;
+                DeviceTypeStr = deviceTypeStr;
+                if (File.TsiFile != null)
+                    DefaultDevice = File.TsiFile.Devices.FirstOrDefault(d => d.TypeStr.Equals(DeviceTypeStr));
+            }
+
+
+            public async Task<Device> LoadAsync()
+            {
+                if (File == null)
+                    return null;
+                var tsi = await File.LoadAsync();
+                if (tsi != null)
+                    DefaultDevice = tsi.Devices.FirstOrDefault(d => d.TypeStr.Equals(DeviceTypeStr));
+                else
+                    DefaultDevice = null;
+                return DefaultDevice;
+            }
+        }
+
+
+        [KnownType(typeof(ControllerDefaultMapping))]
+        [DataContract(Name = "File", Namespace = "")]
+        public class ControllerDefaultMappingFile
+        {
+            [DataMember]
+            public string Manufacturer { get; private set; }
+
+            [DataMember]
+            public string Controller { get; private set; }
+
+            [DataMember]
+            public string Path { get; private set; }
+
+            private List<ControllerDefaultMapping> _defaultMappings = new List<ControllerDefaultMapping>();
+            [DataMember]
+            public List<ControllerDefaultMapping> DefaultMappings { get { return _defaultMappings; } set { _defaultMappings = value; } }
+
+            public TsiFile TsiFile { get; private set; }
+
+
+            public ControllerDefaultMappingFile(string manufacturer, string controller, string path)
+            {
+                Manufacturer = manufacturer;
+                Controller = controller;
+                Path = path;
+            }
+
+            public async Task<TsiFile> LoadAsync()
+            {
+                TsiFile = await Task<TsiFile>.Factory.StartNew(() => TsiFile.Load(CmdrSettings.Instance.TraktorVersion, Path));
+                return TsiFile;
+            }
+        }
+
+
+
+        public static readonly string PATH_TO_CACHE_FOLDER = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "cache");
+        public static readonly string PATH_TO_CACHE_FILE = Path.Combine(PATH_TO_CACHE_FOLDER, "defaultMappings.xml");
+
+        private static Dictionary<string, ControllerDefaultMapping> _dict = new Dictionary<string, ControllerDefaultMapping>();
+        private static int _progressCounter;
+
+
+        private ControllerDefaultMappings() { }
+
+        private static ControllerDefaultMappings _instance;
+        public static ControllerDefaultMappings Instance
+        {
+            get { return _instance ?? (_instance = load() ?? new ControllerDefaultMappings()); }
+        }
+
+
+        public async Task LoadAsync(string rootPath)
+        {            
+            if (String.IsNullOrEmpty(rootPath))
+                return;
+
+            _dict.Clear();
+            _progressCounter = 0;
+
+            App.SetStatus("Loading defaults for proprietary devices ... 0%");
+
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(rootPath);
+                var allFiles = di.EnumerateDirectories().SelectMany(d => d.EnumerateFiles());
+
+                // remove missing files from cache
+                for (int i = Count - 1; i >= 0; i--)
+                {
+                    if (!allFiles.Any(f => f.FullName.Equals(this[i].Path)))
+                        RemoveAt(i);
+                }
+
+                // process and cache new files
+                var files = allFiles.Where(f => !this.Any(ignored => ignored.Path == f.FullName)).ToList();
+                var tasks = files.Select(f => addDefaultMappings(files.Count, f.FullName));
+                await Task.WhenAll(tasks);
+
+                save();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            _dict = this.SelectMany(cdm => cdm.DefaultMappings.Where(dm => dm.DeviceTypeStr != Device.TYPE_STRING_GENERIC_MIDI))
+                .DistinctBy(dm => dm.DeviceTypeStr)
+                .ToDictionary(dm => dm.DeviceTypeStr, dm => dm);
+
+            App.ResetStatus();
+        }
+
+        public ControllerDefaultMapping this[string deviceTypeStr]
+        {
+            get
+            {
+                if (_dict.ContainsKey(deviceTypeStr))
+                    return _dict[deviceTypeStr];
+                return null;
+            }
+        }
+
+
+        private void save()
+        {
+            Directory.CreateDirectory(PATH_TO_CACHE_FOLDER);
+            XDocument doc = SerializationHelper.Serialize<ControllerDefaultMappings>(this, true);
+            doc.Save(PATH_TO_CACHE_FILE);
+        }
+
+        private async Task addDefaultMappings(int count, string pathToDefaultFile)
+        {
+            FileInfo fi = new FileInfo(pathToDefaultFile);
+            string manufacturer = fi.Directory.Name;
+            string controller = Path.GetFileNameWithoutExtension(fi.Name)
+                .Split(new[] { " - " }, StringSplitOptions.RemoveEmptyEntries)
+                .Last();
+
+            TsiFile tsi = await loadTsiAsync(pathToDefaultFile);
+            if (tsi == null)
+            {
+                Debug.WriteLine("Could not load " + pathToDefaultFile);
+                return;
+            }
+
+            var file = new ControllerDefaultMappingFile(manufacturer, controller, pathToDefaultFile);
+            file.DefaultMappings.AddRange (tsi.Devices.Select(pd => new ControllerDefaultMapping(file, pd.TypeStr)));
+            this.Add(file);
+
+            Interlocked.Increment(ref _progressCounter);
+            App.SetStatus("Loading defaults for proprietary devices ... " + ((double)_progressCounter / count * 100) + "%");
+        }
+
+        private static ControllerDefaultMappings load()
+        {
+            if (!File.Exists(PATH_TO_CACHE_FILE))
+                return null;
+
+            ControllerDefaultMappings result = null;
+            try
+            {
+                XDocument doc = XDocument.Load(PATH_TO_CACHE_FILE);
+                result = SerializationHelper.Deserialize<ControllerDefaultMappings>(doc, true);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+
+            if (result != null)
+            {
+                // restore references
+                foreach (var file in result)
+                    foreach (var m in file.DefaultMappings)
+                        m.File = file;
+            }
+
+            return result;
+        }
+
+        private static async Task<TsiFile> loadTsiAsync(string filePath)
+        {
+            return await Task<TsiFile>.Factory.StartNew(() => TsiFile.Load(CmdrSettings.Instance.TraktorVersion, filePath));
+        }
+
+        private static XElement stripNamespace(XElement root)
+        {
+            return new XElement(
+                root.Name.LocalName,
+                root.HasElements ?
+                    root.Elements().Select(el => stripNamespace(el)) :
+                    (object)root.Value
+            );
+        }
+    }
+}
