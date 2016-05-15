092557607be29c11d2819959c236b156d773f4dd
TakTraum_cp <SND\TakTraum_cp>
2016-05-15 19:20:27 +0000

    Added test project for WpfControls

---
+++ cmdr/cmdr.WpfControls.Test/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using cmdr.WpfControls.CustomDataGrid;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cmdr.WpfControls.Test
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var vm = DataContext as ViewModel;
+            var selectionCopy = new List<RowItemViewModel>(vm.SelectedThings);
+            vm.SelectedThings.Clear();
+
+            foreach (var item in selectionCopy)
+            {
+                var copy = (item.Item as cmdr.WpfControls.Test.ViewModel.Thing).Copy();
+                copy.Number *= 2;
+                vm.Things.Add(new CustomDataGrid.RowItemViewModel(copy));
+            }
+        }
+    }
+}
---
+++ cmdr/cmdr.WpfControls.Test/ViewModel.cs
@@ -0,0 +1,90 @@
+﻿using cmdr.WpfControls.CustomDataGrid;
+using cmdr.WpfControls.Utils;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmdr.WpfControls.Test
+{
+    public class ViewModel : ViewModelBase
+    {
+        public class Thing : ViewModelBase
+        {
+            private int _number;
+            public int Number
+            {
+                get { return _number; }
+                set { _number = value; raisePropertyChanged("Number"); }
+            }
+
+            private string _name;
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+
+            public Thing Copy()
+            {
+                return this.MemberwiseClone() as Thing;
+            }
+        }
+
+        private ObservableCollection<RowItemViewModel> _things = new ObservableCollection<RowItemViewModel>();
+        public ObservableCollection<RowItemViewModel> Things
+        {
+            get { return _things; }
+            set { _things = value; raisePropertyChanged("Things"); }
+        }
+
+        private ObservableCollection<RowItemViewModel> _selectedThings = new ObservableCollection<RowItemViewModel>();
+        public ObservableCollection<RowItemViewModel> SelectedThings
+        {
+            get { return _selectedThings; }
+            set { _selectedThings = value; raisePropertyChanged("SelectedThings"); check(); }
+        }
+
+        private ObservableCollection<RowItemViewModel> _highlightedThings = new ObservableCollection<RowItemViewModel>();
+        public ObservableCollection<RowItemViewModel> HighlightedThings
+        {
+            get { return _highlightedThings; }
+            set { _highlightedThings = value; raisePropertyChanged("HighlightedThings"); }
+        }
+
+
+        public ViewModel()
+        {
+            var items = Enumerable.Range(1, 10).Select(i =>
+                new RowItemViewModel
+                {
+                    Item = new Thing
+                    {
+                        Number = i,
+                        Name = "Name " + i,
+                    }
+                }
+            );
+
+            _things = new ObservableCollection<RowItemViewModel>(items);
+
+            SelectedThings.Add(_things[2]);
+            SelectedThings.Add(_things[3]);
+        }
+
+        private void check()
+        {
+            if (SelectedThings == null || SelectedThings.Count == 0)
+                return;
+
+            foreach (var t in Things)
+            {
+                var thing = t.Item as Thing;
+                t.IsHighlighted = thing.Number % ((SelectedThings[0] as RowItemViewModel).Item as Thing).Number == 0;
+            }            
+        }
+    }
+}
